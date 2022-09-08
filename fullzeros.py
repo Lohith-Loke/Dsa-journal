45fcbba1754c790409a0e5878e0e201bda5e5cbf
Lohith <lohith1024@gmail.com>
2022-09-08 17:50:43 +0530

    commit

---
+++ fullzeros.py
@@ -0,0 +1,86 @@
+from tkinter.messagebox import NO
+
+
+class Solution:
+    def makecolzero(self, A, row):
+        for i in range(len(A[0])):
+            A[i][row] = 0
+        return A
+
+    def makerowzero(self, A, clm):
+        for i in range(len(A)):
+            A[clm][i] = 0
+        return A
+    # @param A : list of list of integers
+    # @return the same list modified
+
+    def setZeroes(self, A):
+        row = {}
+        col = {}
+        lncolm = len(A[0])
+        lnrow = len(A[0][0])
+        for i in range(lncolm):
+
+            for j in range(lnrow):
+                print(A[i][j], end=" ")
+                if A[i][j] == 0:
+                    # make a list
+                    if i is row:
+                        pass
+                    else:
+                        row[i] = 0
+                    if j in col:
+                        pass
+                    else:
+                        col[j] = 0
+
+                        # continue
+                # print("(", i, j, end=")")
+            print()
+        print(row)
+        print(col)
+        for i in row:
+            self.makerowzero(A, i)
+        for i in col:
+            self.makecolzero(A, i)
+        return A
+
+
+z = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+],
+a = Solution()
+# x = Solution.makerowzero(b, 0)
+# x = a.makecolzero(b, 0)
+# x = a.setZeroes(z)
+print(type(z))
+
+# for i in z:
+#     print(i)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,2 @@
-print(len(e))
-for i in e:
-    print(i, end=" ")
-print()
-for i in range(0, len(e)-2):
-    print(i, end=" ")
-print()
+for i in z:
+#     print(i)
